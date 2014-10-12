c0df2fea8fa428b6e43486c64a298339c5b5d2b4
Sebastian Krueger Mangelsen <sebastian@kruegermangelsen.com>
2014-10-12 17:23:31 +0200

    fixed indentation issues in unit test file

--- test/LedCtrl_ut.py
+++ test/LedCtrl_ut.py
@@ -12,27 +12,32 @@ from LedCtrl import LedCtrl
 
 class TestSequence(unittest.TestCase):
 
-        def setUp(self):
-                self.ctrl = None
-		GPIO.setmode(GPIO.BCM)
+    def setUp(self):
+        self.ctrl = None
+        GPIO.setmode(GPIO.BCM)
 
 	def tearDown(self):
 		del self.ctrl
 		GPIO.cleanup()
 
-        def test_createLed(self):
-		self.ctrl = LedCtrl()
-		self.ctrl.addLed(17)
-		self.ctrl.addLed(27)
-		self.ctrl.addLed(22)
-                self.assertTrue(isinstance(self.ctrl, LedCtrl))
+    def test_createLed(self):
+        self.ctrl = LedCtrl()
+        self.ctrl.addLed(17)
+        self.ctrl.addLed(27)
+        self.ctrl.addLed(22)
+        self.assertTrue(isinstance(self.ctrl, LedCtrl))
+        
 
 	def test_switchLedOn(self):
 		self.ctrl = LedCtrl()
 		self.ctrl.addLed(17)
 		self.ctrl.addLed(27)
 		self.ctrl.startThread()
+		sleep(3)
 		self.ctrl.sendCmd(17, self.ctrl.LED_ON)
+		sleep(4)
+		self.assertEqual(GPIO.input(17, 1))
+		sleep(1)
 		self.ctrl.stopThread()
 
 if __name__ == "__main__":
